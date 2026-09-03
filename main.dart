7f702be5045d70fd43f9cfb2aaf08ef3b51c8567
asraful0106 <asrafulapf@gmail.com>
2026-09-03 09:39:58 +0600

    feat: convert units done!

--- main.dart
+++ main.dart
@@ -22,3 +22,22 @@ double readTemperature(int choice) {
   String? input = stdin.readLineSync();
   return double.tryParse(input ?? '') ?? 0.0;
 }
+
+double convert(int choice, double temp) {
+  switch (choice) {
+    case 1:
+      return (temp * 9 / 5) + 32;
+    case 2:
+      return (temp - 32) * 5 / 9;
+    case 3:
+      return temp + 273.15;
+    case 4:
+      return temp - 273.15;
+    case 5:
+      return (temp - 32) * 5 / 9 + 273.15;
+    case 6:
+      return (temp - 273.15) * 9 / 5 + 32;
+    default:
+      return 0.0;
+  }
+}
